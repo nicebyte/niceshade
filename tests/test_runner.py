60a302260b9bd3888cbbb28a00cd30c3ef94c307
nicebyte@mailinator.com <nicebyte@gpfault.net>
2019-06-13 00:07:37 -0700

    [Tests] Use `universal_newlines` instead of `text` in subprocess.run

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -41,7 +41,7 @@ def main(argv):
     try:
       run_result = subprocess.run(
           [str(compiler_binary), str(input_file) , "-t", "msl10", "-t", "gl430", "-O", str(out_dir)],
-          capture_output = True, cwd = source_hlsl, timeout = 60, text = True)
+          capture_output = True, cwd = source_hlsl, timeout = 60, universal_newlines = True)
       if not should_fail and run_result.returncode != 0:
         failed_run_results[test_case_name] = "Process exited with nonzero exit code"
       if should_fail and run_result.returncode == 0:
