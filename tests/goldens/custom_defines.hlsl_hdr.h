293d26522792cc892ab3fedb3fa77b5e06145f1a
nicebyte <nicebyte@gpfault.net>
2020-09-19 00:45:40 -0700

    update goldens

--- tests/goldens/custom_defines.hlsl_hdr.h
+++ tests/goldens/custom_defines.hlsl_hdr.h
@@ -1,14 +1,14 @@
 /*auto-generated, do not edit*/
 #pragma once
 namespace simple_texture_def1 {
-  static constexpr int samp_Binding = 2;
-  static constexpr int samp_Set = 0;
   static constexpr int tex1_Binding = 1;
   static constexpr int tex1_Set = 0;
-}
-namespace simple_texture_def2 {
   static constexpr int samp_Binding = 2;
   static constexpr int samp_Set = 0;
+}
+namespace simple_texture_def2 {
   static constexpr int tex2_Binding = 1;
   static constexpr int tex2_Set = 0;
+  static constexpr int samp_Binding = 2;
+  static constexpr int samp_Set = 0;
 }
--- tests/goldens/spec_const_as_array_idx.hlsl_hdr.h
+++ tests/goldens/spec_const_as_array_idx.hlsl_hdr.h
@@ -3,8 +3,8 @@
 namespace blur {
   static constexpr int BlurData_Binding = 1;
   static constexpr int BlurData_Set = 0;
-  static constexpr int bilinearSamp_Binding = 3;
-  static constexpr int bilinearSamp_Set = 0;
   static constexpr int tex_Binding = 2;
   static constexpr int tex_Set = 0;
+  static constexpr int bilinearSamp_Binding = 3;
+  static constexpr int bilinearSamp_Set = 0;
 }
