663f694e3aa6177b9d0501d5845b3c065a5683eb
nicebyte <nicebyte@gpfault.net>
2021-07-16 23:08:02 -0700

    update goldens

--- tests/goldens/blur.vs.430.glsl
+++ tests/goldens/blur.vs.430.glsl
@@ -17,3 +17,9 @@ void main()
     out_var_ATTRIBUTE0 = _33[_38];
 }
 
+/**NGF_NATIVE_BINDING_MAP
+(1 0) : 0
+(2 0) : 0
+(3 0) : 0
+(-1 -1) : -1
+**/
--- tests/goldens/simple_texture.ps.430.glsl
+++ tests/goldens/simple_texture.ps.430.glsl
@@ -10,3 +10,8 @@ void main()
     out_var_SV_TARGET = texture(tex_samp, in_var_ATTRIBUTE0);
 }
 
+/**NGF_NATIVE_BINDING_MAP
+(1 0) : 0
+(2 0) : 0
+(-1 -1) : -1
+**/
