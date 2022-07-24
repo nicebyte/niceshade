875fb4e9f3a9df37ff866b7288859575ce7d862b
Bagrat Dabaghyan <dbagrat@gmail.com>
2022-07-24 23:19:17 +0400

    update goldens

--- tests/goldens/blur.ps.430.glsl
+++ tests/goldens/blur.ps.430.glsl
@@ -4,7 +4,7 @@
 #define SPIRV_CROSS_CONSTANT_ID_0 1u
 #endif
 const uint kernelRadius = SPIRV_CROSS_CONSTANT_ID_0;
-const uint _47[7] = uint[](1u, 4u, 9u, 18u, 29u, 46u, 63u);
+const uint _45[7] = uint[](1u, 4u, 9u, 18u, 29u, 46u, 63u);
 
 layout(binding = 0, std140) uniform type_BlurData
 {
@@ -18,15 +18,15 @@ layout(location = 0) out vec4 out_var_SV_TARGET;
 
 void main()
 {
-    vec4 _51;
-    _51 = vec4(0.0);
-    for (uint _54 = 0u; _54 < _47[kernelRadius]; )
+    vec4 _49;
+    _49 = vec4(0.0);
+    for (uint _52 = 0u; _52 < _45[kernelRadius]; )
     {
-        _51 += (texture(tex_bilinearSamp, in_var_ATTR0 + BlurData.samples[_54].xy) * BlurData.samples[_54].z);
-        _54++;
+        _49 += (texture(tex_bilinearSamp, in_var_ATTR0 + BlurData.samples[_52].xy) * BlurData.samples[_52].z);
+        _52++;
         continue;
     }
-    out_var_SV_TARGET = _51;
+    out_var_SV_TARGET = _49;
 }
 
 /**NGF_NATIVE_BINDING_MAP
--- tests/goldens/fullscreen_triangle.ps.430.glsl
+++ tests/goldens/fullscreen_triangle.ps.430.glsl
@@ -1,6 +1,5 @@
 #version 430
 
-layout(location = 0) in vec2 in_var_ATTRIBUTE0;
 layout(location = 0) out vec4 out_var_SV_TARGET;
 
 void main()
