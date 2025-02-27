c60c214c762a5e93d15e81260134056686256337
Bagrat Dabaghyan <dbagrat@gmail.com>
2025-02-27 10:33:51 +0400

    update dxc libs to v1.8.2502, update goldens, fix test script
    minor formatting fix
    remove unused shaderc_internal_error_FAIL.hlsl
    
    Co-authored-by: Andranik Melikyan <andranikmelikyan@gmail.com>

--- tests/goldens/blur.vs.430.glsl
+++ tests/goldens/blur.vs.430.glsl
@@ -5,7 +5,7 @@ out gl_PerVertex
     vec4 gl_Position;
 };
 
-const vec4 _29[3] = vec4[](vec4(-1.0, -1.0, 0.0, 1.0), vec4(3.0, -1.0, 0.0, 1.0), vec4(-1.0, 3.0, 0.0, 1.0));
+const vec4 _32[3] = vec4[](vec4(-1.0, -1.0, 0.0, 1.0), vec4(3.0, -1.0, 0.0, 1.0), vec4(-1.0, 3.0, 0.0, 1.0));
 const vec2 _33[3] = vec2[](vec2(0.0), vec2(2.0, 0.0), vec2(0.0, 2.0));
 
 layout(location = 0) out vec2 out_var_ATTRIBUTE0;
@@ -13,7 +13,7 @@ layout(location = 0) out vec2 out_var_ATTRIBUTE0;
 void main()
 {
     uint _38 = uint(gl_VertexID) % 3u;
-    gl_Position = _29[_38] * 1.0;
+    gl_Position = _32[_38] * 1.0;
     out_var_ATTRIBUTE0 = _33[_38];
 }
 
